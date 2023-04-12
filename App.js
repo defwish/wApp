09c1d783fcaa1cca65f56e5393691e9f9e3b194e
defwish <58536015+defwish@users.noreply.github.com>
2023-04-12 06:55:42 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,1 @@
-import React from "react";
-import Weather from "./Weather";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
-};
-
-export default App;
